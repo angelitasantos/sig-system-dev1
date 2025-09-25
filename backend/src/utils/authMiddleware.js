106b1f200d3c3c79a9879718485e812aafc2c246
Angelita Santos <angelitasantos@gmail.com>
2025-09-25 12:24:26 -0300

    chore: cria middleware para validar JWT em rotas protegidas #1

---
+++ backend/src/utils/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const apiResponse = require('./apiResponse');
+
+function authMiddleware(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return apiResponse.error(res, 'Token não fornecido!', 401);
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return apiResponse.error(res, 'Token inválido ou expirado!', 403, [err.message]);
+    }
+}
+
+module.exports = authMiddleware;
